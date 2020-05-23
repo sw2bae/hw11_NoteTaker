30fe3b565fe7fe4ba97e0011569441f3a9475447
Sang Bae <sw2.bae@gmail.com>
2020-05-23 17:54:10 -0500

    rev03_GET HTMLs

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require ("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const path = require("path");
+const fs = require("fs");
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+
+// html
+app.get("/",(req,res)=>res.sendFile(path.join(__dirname,"./public/index.html")));
+app.get("/notes",(req,res)=>res.sendFile(path.join(__dirname,"./public/notes.html")));
+
+
+
 
 app.listen(PORT,()=> console.log("App listening on Port " + PORT));
