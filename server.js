178ab9970a4cfb7b1ed9b66d45dfe04218493f6e
Sang Bae <sw2.bae@gmail.com>
2020-05-23 19:13:43 -0500

    rev04_staticPublic Added for CSS

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require ("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const path = require("path");
-const fs = require("fs");
+// const fs = require("fs");
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+// CSS
+app.use(express.static('public'));
 
 // html
 app.get("/",(req,res)=>res.sendFile(path.join(__dirname,"./public/index.html")));
 app.get("/notes",(req,res)=>res.sendFile(path.join(__dirname,"./public/notes.html")));
+
+
 
 
 
