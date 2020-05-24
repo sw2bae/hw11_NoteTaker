1606af16d59ee084ea511cc84471b164ec1ccc3b
Sang Bae <sw2.bae@gmail.com>
2020-05-24 16:27:48 -0500

    rev06_db.json connected

--- server.js
+++ server.js
@@ -2,29 +2,37 @@ const express = require ("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const path = require("path");
-// const fs = require("fs");
+const fs = require("fs");
+const db = require("./db/db.json");
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 // assets
 app.use(express.static('public'));
 
-const saveNotes = []; 
-
 // html
 app.get("/",(req,res)=>res.sendFile(path.join(__dirname,"./public/index.html")));
 app.get("/notes",(req,res)=>res.sendFile(path.join(__dirname,"./public/notes.html")));
 
 
+
 //API_POST
 app.post("/api/notes",(req,res)=>{
     const newNotes = req.body;
-    saveNotes.push(newNotes);
-    res.json(newNotes);
+    newNotes.id = db.length
+    db.push(newNotes);
+    res.json(db);
+    fs.writeFile("./db/db.json",JSON.stringify(db),(err)=>{
+        if (err) {
+            return console.log(err);
+          }
+          console.log("Success!");
+    });
 });
 
 // API_GET
-app.get("/api/notes",(req,res)=>res.json(saveNotes));
+app.get("/api/notes",(req,res)=>res.json(db));
+console.log(db);
 
 // app.get("/api/notes/:id", function(req, res) {
 //     var id = req.params.id;
